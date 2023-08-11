b4480ef5d3b11633f1722b95ad68544b1317ce65
Zakarialabib <Zakarialabib@users.noreply.github.com>
2023-08-11 23:38:34 +0000

    Fix code styling

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -13,7 +13,7 @@ class SettingController extends Controller
     {
         return view('admin.settings.index', compact('setting'));
     }
-    
+
     public function redirects()
     {
         return view('admin.settings.redirects');
