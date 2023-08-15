1d8f8ee3619e70e1df61f7797e0eba98976df7df
Zakaria Labib <58374576+Zakarialabib@users.noreply.github.com>
2023-08-15 19:00:49 +0100

    improving OrderType

--- app/Enums/OrderType.php
+++ app/Enums/OrderType.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Enums;
 
+use Illuminate\Support\Str;
+
 enum OrderType: string
 {
     case REGISTRATION = '0';
@@ -15,5 +17,26 @@ enum OrderType: string
     public static function values(): array
     {
         return array_column(self::cases(), 'name', 'value');
+    }
+
+    public function getName(): string
+    {
+        return __(Str::studly($this->name));
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    public static function getLabel($value): ?string
+    {
+        foreach (self::cases() as $case) {
+            if ($case->getValue() === $value) {
+                return $case->getName();
+            }
+        }
+
+        return null;
     }
 }
