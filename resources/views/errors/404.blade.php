4e0572410b4f73f5baae2376f8ffc5183a9c7d68
Zakaria Labib <58374576+Zakarialabib@users.noreply.github.com>
2023-08-15 18:59:54 +0100

    error views improvements

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,5 @@
-@extends('errors::minimal')
+@extends('errors::illustrated-layout')
 
-@section('title', __('Not Found'))
+@section('title', __('Page not found'))
 @section('code', '404')
-@section('message', __('Not Found'))
+@section('message', __('Page not found'))
---
+++ resources/views/errors/illustrated-layout.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html x-data="mainState" class="scroll-smooth" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Head Tags -->
+    @if (Helpers::settings('head_tags'))
+        {!! Helpers::settings('head_tags') !!}
+    @endif
+
+    <title>
+        @yield('title') || {{ Helpers::settings('site_title') }}
+    </title>
+ 
+    <link rel="icon" href="{{ asset('images/' . Helpers::settings('site_favicon')) }}" type="image/x-icon">
+
+    <!-- Styles -->
+    @include('partials.front.css')
+
+</head>
+
+<body class="antialiased font-sans">
+    <!-- Body Tags -->
+    @if (Helpers::settings('body_tags'))
+        {!! Helpers::settings('body_tags') !!}
+    @endif
+
+    <x-header />
+
+    <section class="relative py-24 md:py-44 lg:py-64 bg-white">
+        <div class="relative z-10 px-4 mx-auto">
+            <div class="max-w-4xl mx-auto text-center">
+                <span
+                    class="inline-block py-px px-2 mb-4 text-xs leading-5 text-green-500 bg-green-100 font-medium rounded-full shadow-sm">@yield('code', __('Oh no'))</span>
+                <h2 class="mb-4 text-4xl md:text-5xl leading-tight font-bold tracking-tighter">
+                    {{ __('Page not found') }}</h2>
+                <p class="mb-12 text-lg md:text-xl text-gray-500">@yield('message')</p>
+                <div class="flex flex-wrap justify-center">
+                    <div class="w-full md:w-auto py-1">
+                        <x-button primary
+                            class="inline-block py-5 px-7 w-full text-base md:text-lg leading-4 text-green-50 font-medium text-center bg-green-500 hover:bg-green-600 focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 border border-green-500 rounded-md shadow-sm"
+                            href="{{ app('router')->has('dashboard') ? route('dashboard') : url('/') }}">
+                            {{ __('Go back to Homepage') }}
+                        </x-button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <x-theme.footer />
+    @include('partials.front.js')
+</body>
+
+</html>
